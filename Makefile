30efedd6c3747f78c1deda84c2d9fbd60d9f3a83
M. Joshua Ryan <luser.droog@gmail.com>
2020-12-24 12:03:49 -0600

    reorganize. add tim and anton test cases

--- Makefile
+++ Makefile
@@ -1,1 +1,1 @@
-CFLAGS= -std=c99 -g -Wall -Wpedantic -Wextra -Wno-unused-function -Wno-unused-parameter -Wno-switch -Wreturn-type -Wunused-variable
+CFLAGS= -std=c99 -g -Wall -Wpedantic -Wextra -Wno-unused-function -Wno-unused-parameter -Wno-switch -Wreturn-type -Wunused-variable $(cflags)
--- strpcom-v5.c
+++ strpcom-v5.c
@@ -1,20 +1,23 @@
 //strpcom-v5.c:
 //$ make strpcom-v5 CFLAGS='-std=c99 -Wall -pedantic -Wextra -Wno-switch'
-#define DEBUG(...) __VA_ARGS__
-//#define DEBUG(...)
-#define HANDLE_ROOTS(...) __VA_ARGS__
-//#define HANDLE_ROOTS(...)
-#define FINAL_GC(...) __VA_ARGS__
-//#define FINAL_GC(...)
+
+#ifndef DEBUG_LEVEL
+#define DEBUG_LEVEL 0
+#endif
+static const int debug_level = DEBUG_LEVEL;
+static const int handle_roots = 1;
+
 
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+
 typedef union uobject *object;
 typedef object list;
 typedef enum tag { INVALID, INTEGER, LIST } tag;
+#define OBJECT(...) new_( (union uobject[]){{ __VA_ARGS__ }} )
 
 union uobject { tag t;
        struct { tag t; int i, continues; } Int;
@@ -29,26 +32,251 @@ typedef struct record {
 } record;
 
 
-list cons( object a, object b );
-list skip_quote( object q, list o );
-list nested_comment( list o );
-void print( list o );
-
-
 record *allocation_list;
 object global_roots;
-object add_global_root( object o, object *op ){
-  HANDLE_ROOTS(
-                global_roots = cons( o, global_roots );
-		record *r = ((void*)( (char*)o - offsetof( record, o ) ) );
-		r->handle = op;
-              )
+
+static list     chars_from_file( FILE *f );
+static list     logical_lines( list o );
+static list     restore_continues( list o );
+static list     strip_comments( list o );
+static list     skip_quote( object q, list o );
+static list     nested_comment( list o );
+static void     print( list o, FILE *f );
+
+static object   Int( int i );
+static list     cons( object a, object b );
+static list     one( object a );
+static object   car( list o );
+static object   cdr( list o );
+static int      eq( object a, object b );
+static int      eqint( object a, int i );
+static int      match( object pat, object it, object *matched, object *tail );
+
+static object   add_global_root( object o, object *op );
+static int      collect( object local_roots );
+static void       mark( object ob );
+static int        sweep( record **ptr );
+static record *   alloc();
+static object   new_( object a );
+static void     error( char *msg );
+
+
+int
+main( int argc, char **argv ){
+  list input = chars_from_file( argc > 1 ? fopen( argv[1], "r" )  : stdin );
+  if(  debug_level >= 2  ) fprintf( stderr, "input:\n"), print( input, stderr );
+
+  list logical = logical_lines( input );
+  if(  debug_level >= 2  ) fprintf( stderr, "logical:\n"), print( logical, stderr );
+
+  list stripped = strip_comments( logical );
+  if(  debug_level >= 2  ) fprintf( stderr, "stripped:\n"), print( stripped, stderr );
+
+  list restored = restore_continues( stripped );
+  if(  debug_level >= 2  ) fprintf( stderr, "restored:\n");
+
+  print( restored, stderr );
+
+  if(  debug_level >= 2  ) fprintf( stderr, "@%d\n", collect( restored ) );
+  if(  debug_level  ){
+    if(  handle_roots  ){ global_roots = NULL; }
+    fprintf( stderr, "@%d\n", collect( NULL ) );
+    input = logical = stripped = restored = NULL;
+  }
+}
+
+list
+chars_from_file( FILE *f ){
+  int c = fgetc( f );
+  return  c != EOF  ? cons( Int( c ), chars_from_file( f ) )
+                    : one( Int( c ) );
+}
+
+list
+logical_lines( list o ){
+  if(  !o  ) return  NULL;
+  static list pat;
+  if(  !pat  )
+    pat = add_global_root( cons( Int( '\\' ), one( Int( '\n' ) ) ), &pat );
+
+  object matched, tail;
+  if(  debug_level >= 2  )
+    if( car(o)->Int.i!=EOF )
+      fprintf( stderr, "[%c%c]", car(o)->Int.i, car(cdr(o))->Int.i );
+  if(  match( pat, o, &matched, &tail )  ){
+    if(  debug_level >= 2  ) fprintf( stderr, "@" );
+    car( tail )->Int.continues = car( o )->Int.continues + 1;
+    return  logical_lines( tail );
+  } else {
+    object a = car( o );
+    return  cons( a, logical_lines( cdr( o ) ) );
+  }
+}
+
+list
+restore_continues( list o ){
+  if(  !o  ) return  NULL;
+  object a = car( o );
+  object z = cdr( o );
+  object r = cons( a, restore_continues( z ) );
+  while(  a->Int.continues  ){
+    r = cons( Int( '\\' ), cons( Int( '\n' ), r ) );
+    --a->Int.continues;
+  }
+  return  r;
+}
+
+list
+strip_comments( list o ){
+  if(  !o  ) return  NULL;
+  static list single, multi;
+  if(  !single  )
+    single = add_global_root( cons( Int('/'), one(Int('/') ) ), &single );
+  if(  !multi   )
+    multi = add_global_root( cons( Int('/'), one(Int('*') ) ), &multi );
+
+  object matched, tail;
+  if(  debug_level >= 2 && car(o)->Int.i != EOF  )
+      fprintf( stderr, "<%c%c>", car(o)->Int.i, car(cdr(o))->Int.i );
+  if(  match( single, o, &matched, &tail )  ){
+    if(  debug_level >= 2  ) fprintf( stderr, "@" );
+    object c;
+    for(  c = car( tail );
+          tail && ! (eqint( c, '\n' ) || eqint( c, EOF ));
+	  c = car( tail = cdr( tail ) )  )
+      ;
+    return  cons( Int( '\n' ),
+                  strip_comments( cdr( tail ) ) );
+  } else if(  match( multi, o, &matched, &tail )  ){
+    if(  debug_level >= 2  ) fprintf( stderr, "@" );
+    return  cons( Int( ' ' ),
+                  strip_comments( nested_comment( cdr( tail ) ) ) );
+  }
+
+  object a = car( o );
+  if(  eqint( a, '\'' ) || eqint( a, '"' )  )
+    return  cons( a,
+                  skip_quote( a, cdr( o ) ) );
+  return  cons( a,
+                strip_comments( cdr( o ) ) );
+}
+
+list
+nested_comment( list o ){
+  if(  !o  ) error( "Unterminated comment\n" );
+  if(  debug_level >= 2  )
+    fprintf( stderr, "(%c%c)", car( o )->Int.i, car( cdr( o ) )->Int.i );
+  static list end;
+  if(  !end  )
+    end = add_global_root( cons( Int( '*' ), one( Int( '/' ) ) ), &end ); 
+
+  object matched, tail;
+  if(  match( end, o, &matched, &tail )  ) return  tail;
+  if(  eqint( car( o ), EOF )  ) error( "Unterminated comment\n" );
+  return  nested_comment( cdr( o ) );
+}
+
+list
+skip_quote( object q, list o ){
+  if(  !o  ) error( "Unterminated literal\n" );
+  object a = car( o );
+  if(  eqint( a, '\\' )  )
+    return  cons( a,
+                  cons( car( cdr( o ) ),
+                        skip_quote( q, cdr( cdr( o ) ) ) ) );
+  else if(  eq( a, q )  )
+    return  cons( a,
+                  strip_comments( cdr( o ) ) );
+  if(  eqint( a, EOF )  ) error( "Unterminated literal\n" );
+  return  cons( a,
+                skip_quote( q, cdr( o ) ) );
+}
+
+void
+print( list o, FILE *f ){
+  switch(  o  ? o->t  : 0  ){
+    case INTEGER: if(  o->Int.i != EOF  ) fputc( o->Int.i, f );
+                  break;
+    case LIST: print( car( o ), f );
+               print( cdr( o ), f ); break;
+  }
+}
+
+
+object
+Int( int i ){
+  return  OBJECT( .Int = { INTEGER, i } );
+}
+
+list
+cons( object a, object b ){
+  return  OBJECT( .List = { LIST, a, b } );
+}
+
+list
+one( object a ){
+  return  cons( a, NULL );
+}
+
+object
+car( list o ){
+  return  o  && o->t == LIST  ? o->List.a  : NULL;
+}
+
+object
+cdr( list o ){
+  return  o  && o->t == LIST  ? o->List.b  : NULL;
+}
+
+int
+eq( object a, object b ){
+  return  !a && !b         ? 1                     :
+          !a || !b         ? 0                     :
+          a->t != b->t     ? 0                     :
+          a->t == INTEGER  ? a->Int.i == b->Int.i  :
+          !memcmp( a, b, sizeof *a );
+}
+
+int
+eqint( object a, int i ){
+  union uobject b = { .Int = { INTEGER, i } };
+  return  eq( a, &b );
+}
+
+int
+match( object pat, object it, object *matched, object *tail ){
+  if(  !pat  ) return  *tail = it,  1;
+  if(  pat->t != (it  ? it->t  : 0)  ) return  0;
+  switch(  pat->t  ){
+    case LIST:
+      {
+        object sink;
+        if(  match( car( pat ), car( it ), & sink, tail )  ){
+          return  *matched = it,
+                  match( cdr( pat ), cdr( it ), & sink, tail );
+        } 
+      } break;
+    case INTEGER:
+      if(  eq( pat, it )  ){
+        return  *matched = it,  1;
+      } 
+  }
+  return  0;
+}
+
+
+object
+add_global_root( object o, object *op ){
+  if(  handle_roots  ){
+    global_roots = cons( o, global_roots );
+    record *r = ((void*)( (char*)o - offsetof( record, o ) ) );
+    r->handle = op;
+  }
   return  o;
 }
-record *alloc(){
-  return  calloc( 1, sizeof(record) );
-}
-void mark( object ob ){
+
+void
+mark( object ob ){
   if(  !ob  ) return;
   record *r = ((void*)( (char*)ob - offsetof( record, o ) ) );
   if(  r->mark  ) return;
@@ -57,7 +285,9 @@ void mark( object ob ){
     case LIST: mark( ob->List.a ); mark( ob->List.b ); break;
   }
 }
-int sweep( record **ptr ){
+
+int
+sweep( record **ptr ){
   int count = 0;
   while(  *ptr  ){
     if(  (*ptr)->mark  ){
@@ -73,14 +303,21 @@ int sweep( record **ptr ){
   }
   return  count;
 }
-int collect( object local_roots ){
+
+int
+collect( object local_roots ){
   mark( local_roots );
   mark( global_roots );
   return  sweep( &allocation_list );
 }
 
-#define OBJECT(...) new_( (union uobject[]){{ __VA_ARGS__ }} )
-object new_( object a ){
+record *
+alloc(){
+  return  calloc( 1, sizeof(record) );
+}
+
+object
+new_( object a ){
   record *r = alloc();
   object p = NULL;
   if(  r  ){
@@ -92,161 +329,8 @@ object new_( object a ){
   return  p;
 }
 
-object Int( int i ){ return  OBJECT( .Int = { INTEGER, i } ); }
-list cons( object a, object b ){ return  OBJECT( .List = { LIST, a, b } ); }
-list one( object a ){ return  cons( a, NULL ); }
-object car( list o ){ return  o  && o->t == LIST  ? o->List.a  : NULL; }
-object cdr( list o ){ return  o  && o->t == LIST  ? o->List.b  : NULL; }
-
-int eq( object a, object b ){
-  return  !a && !b                    ? 1  :
-          !a || !b                    ? 0  :
-          a->t != b->t                ? 0  :
-          a->t == INTEGER  ? a->Int.i == b->Int.i
-                           : !memcmp( a, b, sizeof *a );
-}
-
-int eqint( object a, int i ){
-  union uobject b = { .Int = { INTEGER, i } };
-  return  eq( a, &b );
-}
-
-int match( object pat, object it, object *matched, object *tail ){
-  if(  !pat  ){
-    return  *tail = it,  1;
-  }
-  if(  pat->t != (it  ? it->t  : 0)  ) return  0;
-  switch(  pat->t  ){
-    case LIST:
-      {
-        object sink;
-        if(  match( car( pat ), car( it ), & sink, tail )  ){
-          return  *matched = it,  match( cdr( pat ), cdr( it ), & sink, tail );
-        } 
-      } break;
-    case INTEGER:
-      if(  eq( pat, it )  ){
-        return  *matched = it,  1;
-      } 
-  }
-  return  0;
-}
-
-list chars_from_file( FILE *f ){
-  int c = fgetc( f );
-  return  c != EOF  ? cons( Int( c ), chars_from_file( f ) )  : one( Int( c ) );
-}
-
-list logical_lines( list o ){
-  static list pat;
-  if(  !pat  ) pat = add_global_root( cons( Int( '\\' ), one( Int( '\n' ) ) ), &pat );
-
-  object matched, tail;
-  DEBUG( if( car(o)->Int.i!=EOF )
-         fprintf( stderr, "[%c%c]", car(o)->Int.i, car(cdr(o))->Int.i ); )
-  if(  match( pat, o, &matched, &tail )  ){
-    DEBUG( fprintf( stderr, "@" ); )
-    car( tail )->Int.continues = car( o )->Int.continues + 1;
-    return  logical_lines( tail );
-  } else {
-    object a = car( o );
-    return  eqint( a, EOF )  ? o  : cons( a, logical_lines( cdr( o ) ) );
-  }
-}
-
-list restore_continues( list o ){
-  if(  !o  ) return  NULL;
-  object a = car( o );
-  if(  eqint( a, EOF )  ) return  o;
-  object z = cdr( o );
-  object r = cons( a, restore_continues( z ) );
-  while(  a->Int.continues  ){
-    r = cons( Int( '\\' ), cons( Int( '\n' ), r ) );
-    --a->Int.continues;
-  }
-  return  r;
-}
-
-list strip_comments( list o ){
-  static list single, multi;
-  if(  !single  ) single = add_global_root( cons( Int('/'), one(Int('/')) ), &single );
-  if(  !multi   ) multi = add_global_root( cons( Int('/'), one(Int('*')) ), &multi );
-
-  object matched, tail;
-  DEBUG( if(car(o)->Int.i!=EOF)
-          fprintf(stderr,"<%c%c>", car(o)->Int.i, car(cdr(o))->Int.i); )
-  if(  match( single, o, &matched, &tail )  ){
-    DEBUG( fprintf( stderr, "@" ); )
-    for(  matched = car( tail ), tail = cdr( tail );
-          !eqint( matched, '\n' ) && !eqint( matched, EOF );
-	  matched = car( tail ), tail = cdr( tail )  )
-      ;
-    return  strip_comments( tail );
-  } else if(  match( multi, o, &matched, &tail )  ){
-    DEBUG( fprintf( stderr, "@" ); )
-    return  cons( Int( ' ' ),
-                  strip_comments( nested_comment( cdr( tail ) ) ) );
-  }
-
-  object a = car( o );
-  if(  eqint( a, '\'' ) || eqint( a, '"' )  )
-    return  cons( a, skip_quote( a, cdr( o ) ) );
-  return  eqint( a, EOF )  ? o
-                           : cons( a, strip_comments( cdr( o ) ) );
-}
-
-list nested_comment( list o ){
-  DEBUG(fprintf( stderr, "(%c%c)", car( o )->Int.i, car( cdr( o ) )->Int.i );)
-  static list end;
-  if(  !end  ) end = add_global_root( cons( Int( '*' ), one( Int( '/' ) ) ), &end ); 
-
-  object matched, tail;
-  if(  match( end, o, &matched, &tail )  )
-    return  tail;
-  if(  eqint( car( o ), EOF )  )
-    fprintf( stderr, "Unterminated comment\n"),
-    exit( 1 );
-  return  nested_comment( cdr( o ) );
-}
-
-list skip_quote( object q, list o ){
-  object a = car( o );
-  if(  eqint( a, '\\' )  ){
-    return  cons( a, cons( car( cdr( o ) ),
-                           skip_quote( q, cdr( cdr( o ) ) ) ) );
-  } else if(  eq( a, q )  ){
-    return  cons( a, strip_comments( cdr( o ) ) );
-  }
-  if(  eqint( a, EOF )  )
-    fprintf( stderr, "Unterminated literal\n"),
-    exit( 1 );
-  return  cons( a, skip_quote( q, cdr( o ) ) );
-}
-
-void print( list o ){
-  switch(  o  ? o->t  : 0  ){
-    case INTEGER: if(  o->Int.i != EOF  ) fputc( o->Int.i, stdout );
-                  break;
-    case LIST: print( car( o ) );
-               print( cdr( o ) ); break;
-  }
-}
-
-
-int main( int argc, char **argv ){
-  list input = chars_from_file( argc > 1 ? fopen( argv[1], "r" )  : stdin );
-  DEBUG( printf( "input:\n"); print( input ); )
-  list logical = logical_lines( input );
-  DEBUG( printf( "logical:\n"); print( logical ); )
-  list stripped = strip_comments( logical );
-  DEBUG( printf( "stripped:\n"); print( stripped ); )
-  list restored = restore_continues( stripped );
-  DEBUG( printf( "restored:\n"); )
-  print( restored );
-  DEBUG( printf( "@%d\n", collect( restored ) ); )
-  FINAL_GC(
-	 HANDLE_ROOTS( global_roots = NULL; )
-         printf( "@%d\n", collect( NULL ) );
-         input = logical = stripped = restored = NULL;
-         )
+void
+error( char *msg ){
+  fprintf( stderr, "%s", msg );
+  exit( EXIT_FAILURE );
 }
